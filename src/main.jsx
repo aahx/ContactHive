7cb9d06f6db592dde8c9592603cf5c34ca57c46f
sparklingwaterlemon <michaelkim009@gmail.com>
2023-04-09 18:59:13 -0700

    created Contact.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
 import './index.css'
 import Root from "./routes/root";
 import ErrorPage from './error';
+import Contact from './routes/contact';
 
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(

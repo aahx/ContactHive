bd69f630d1762f112961eb3b5a8c23dda6d5e843
sparklingwaterlemon <michaelkim009@gmail.com>
2023-04-12 15:21:38 -0700

    updated index with personal info

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,27 @@
 export default function Index(){
     return(
+        <>
+        <p id="double-zero-state">
+            Created by Michael Kim 
+            <br />
+            April 2023
+            <br />
+            <br />
+            Check out this repo at
+            <br />
+            <a href="https://github.com/sparklingwaterlemon/Contact-List" target="_blank">
+                github.com/sparklingwaterlemon
+            </a>
+            <br />
+            <br />
+            Connect with me on LinkedIn
+            <br />
+            <a href="https://www.linkedin.com/in/michaelkim3/" target="_blank">
+                linkedin.com/in/michaelkim3
+            </a>
+        </p>
         <p id="zero-state">
-            This is a demo for React React.
+            This is the demo for React Router v6.10.
             <br />
             Check out{" "}
             <a href="https://reactrouter.com">
@@ -9,5 +29,6 @@ export default function Index(){
             </a>
             .
         </p>
+        </>
     );
 }
